668ab9bc95b90865dea4a6f7cd8b8e8b45a93f0f
Nishant Bhagat <nishantbhagat57@gmail.com>
2023-10-15 02:42:00 +0530

    Update gen1.py

--- gen1.py
+++ gen1.py
@@ -49,7 +49,7 @@ def main(urls_file, extensions_file):
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: python url_wordlist_generator.py <urls_file> <extensions_file>")
+        print("Usage: python gen1.py <urls_file> <extensions_file>")
         sys.exit(1)
 
     main(sys.argv[1], sys.argv[2])
