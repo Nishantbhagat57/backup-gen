be77cfca5bf8689fe5b3b08820ee2c309fcf42b2
Nishant Bhagat <nishantbhagat57@gmail.com>
2023-10-20 21:49:41 +0530

    Update gen.py

--- gen.py
+++ gen.py
@@ -1,5 +1,6 @@
 import sys
 import tldextract
+import argparse
 
 def combinations(words, symbol):
     words_split = words.split(symbol)
@@ -10,7 +11,7 @@ def concat_extensions(results, extensions):
     extended_results = [''.join([result, extension]) for result in results for extension in extensions]
     return extended_results
 
-def parse_url(url, extensions):
+def parse_url(url, extensions, full_url=False):
     parsed_url = tldextract.extract(url)
     scheme = url.split(":")[0]
 
@@ -36,6 +37,9 @@ def parse_url(url, extensions):
     results.append(words_Z_dot[-1])
     results.append(company_name_Z)
 
+    # Check if the full_url flag is true then append the scheme to results
+    if full_url:
+        results = [scheme + '://' + url + '/' + i for i in results]
     # Concate results with extensions
     results = concat_extensions(results, extensions)
 
@@ -48,15 +52,25 @@ def get_extensions(ext_filepath):
         extensions = ['.' + ext if ext[0] != '.' else ext for ext in extensions]
         return extensions
 
-def main(url_file_path, ext_filepath):
-    extensions = get_extensions(ext_filepath)
-    with open(url_file_path, 'r') as url_file:
+
+def main():
+    # Using argparse to handle CLI arguments
+    parser = argparse.ArgumentParser(description='URL Wordlist Generator')
+    parser.add_argument('url_file', type=str, help='The file path for the URLs')
+    parser.add_argument('ext_file', type=str, help='The file path for the extensions')
+    parser.add_argument('--full', action='store_true', help='Include full urls with http/https scheme')
+
+    args = parser.parse_args()
+
+    extensions = get_extensions(args.ext_file)
+
+    with open(args.url_file, 'r') as url_file:
         for url in url_file:
             url = url.strip()  # Removing leading/trailing whitespaces, if any
             if url:  # In case there are empty lines within file, just skip it
-                results = sorted(list(set(parse_url(url, extensions))))
+                results = sorted(list(set(parse_url(url, extensions, args.full))))
                 print("\n".join(results))
 
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2])
+    main()
